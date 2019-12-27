7b5e34938e3b768a86dd6ac6cecf12b696f6ac93
bill.wang <bill.wang@edgentauems.com>
2019-12-27 11:36:34 +0800

    第二節練習

---
+++ MoshMVC/Controllers/CustomersController.cs
@@ -0,0 +1,37 @@
+﻿using MoshMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoshMVC.Controllers
+{
+    public class CustomersController : Controller
+    {
+        readonly IEnumerable<Customer> customers;
+
+        public CustomersController()
+        {
+            customers = new List<Customer>() 
+            {
+                new Customer{ Id = 1, Name = "Bill"},
+                new Customer{ Id = 2, Name = "William"}
+            };
+        }
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(customers);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
---
+++ MoshMVC/Controllers/MoviesController.cs
@@ -0,0 +1,28 @@
+﻿using MoshMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoshMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        readonly IEnumerable<Movie> movies;
+
+        public MoviesController()
+        {
+            movies = new List<Movie>() 
+            {
+                new Movie{ Id = 1, Name = "Mission Impossible"},
+                new Movie{ Id = 2, Name = "Mission Impossible 2"},
+            };
+        }
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View(movies);
+        }
+    }
+}

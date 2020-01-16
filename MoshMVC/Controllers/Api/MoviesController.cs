137e9531d471c124f707a344d7fd6dbfcff88e99
bill.wang <bill.wang@edgentauems.com>
2020-01-16 16:08:57 +0800

    add api controller and dto.cs

---
+++ MoshMVC/Controllers/Api/MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using MoshMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MoshMVC.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private EFDbContext _dbContext;
+        public MoviesController()
+        {
+            _dbContext = new EFDbContext();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            var movies = _dbContext.Movies.ToList();
+
+            if (movies == null)
+                return NotFound();
+
+            return Ok(movies);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            var movie = _dbContext.Movies
+                .FirstOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return NotFound();
+
+            return Ok(movie);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(Movie movie)
+        {
+
+        }
+    }
+}
---
+++ MoshMVC/Dtos/MovieDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MoshMVC.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int GenreId { get; set; }
+
+        public byte NumberInStock { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+    }
+}
